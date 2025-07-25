d4d84b114a21e5495300cfa696bb9d8b772dca28
Anouar Bensalem <anouar.bensalem.etd@univ-tiaret.dz>
2025-07-25 23:49:35 +0100

    JavaScript Objects

--- 37.JsObjects/JsObjects.js
+++ 37.JsObjects/JsObjects.js
@@ -2,3 +2,37 @@
 //          can represent real world objects (people,products,places)
 //          Objects = {key:value,
 //                      function()}
+
+// we will create a person1 object
+
+const person1 =  {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age: 30,
+    isEmployed: true,
+    sayHello: function(){console.log("Hi! I am  Spongebob")},
+    eat: function(){console.log("I am eating a Krabby Patty!")},
+}
+// obejects can't have the same name 
+// the properties mimick real worl attributes of a person like name age ....
+
+
+const person2 = {
+    firstName: "Patrick",
+    lastName: "Star",
+    age: 42,
+    isEmployed: false,
+    sayHello: function(){console.log("Hi! I'm Patrick ..." )},
+    eat: () => console.log("I am eating roast beef, chiken, and pizza"),
+}
+person1.eat();
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.age);
+console.log(person2.isEmployed);
+
+person2.sayHello();
+person2.eat();
+
+
